3e6df09c0d42a66ce6d58b81892f28b9431e2bcc
Martin Zlámal <mrtnzlml@gmail.com>
2021-03-21 20:59:26 -0600

    SX Design: add new Emoji component
    adeira-source-id: 49699de795c7e03182be2a372dd6938aa943cde7

---
+++ src/Emoji/Emoji.js
@@ -0,0 +1,20 @@
+// @flow strict
+
+import * as React from 'react';
+
+type Props = {|
+  +label: FbtWithoutString,
+  +symbol: string,
+|};
+
+export default function Emoji(props: Props): React.Node {
+  return (
+    <span
+      role="img"
+      aria-label={props.label ? props.label : ''}
+      aria-hidden={props.label ? 'false' : 'true'}
+    >
+      {props.symbol}
+    </span>
+  );
+}
